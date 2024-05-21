70812acbc489d5eaf44ffa064d0ff81759be108d
SalvadorGalvez <salva01797@gmail.com>
2024-05-20 20:08:47 -0600

    Etiquetado de rutas Swagger en TaskController

--- src/main/java/com/galvez/salvador/backend/controllers/TaskController.java
+++ src/main/java/com/galvez/salvador/backend/controllers/TaskController.java
@@ -2,6 +2,8 @@ package com.galvez.salvador.backend.controllers;
 
 import com.galvez.salvador.backend.models.Task;
 import com.galvez.salvador.backend.services.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,31 +11,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/tasks")
+@Tag(name = "Tareas", description = "API para gestionar tareas")
 public class TaskController {
     @Autowired
     private TaskService taskService;
 
     @GetMapping
+    @Operation(summary = "Obtener la lista de tareas", tags = {"Tareas"})
     public List<Task> index() {
         return taskService.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener tarea por id", tags = {"Tareas"})
     public Task show(@PathVariable("id") Long id) {
         return taskService.findById(id);
     }
 
     @PostMapping
+    @Operation(summary = "Crear tarea nueva", tags = {"Tareas"})
     public Task create(@RequestBody() Task task) {
         return taskService.create(task);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Editar tarea", tags = {"Tareas"})
     public Task update(@PathVariable("id") Long id, @RequestBody() Task task) {
         return taskService.update(id, task);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar tarea", tags = {"Tareas"})
     public void delete(@PathVariable("id") Long id) {
         taskService.delete(id);
     }
